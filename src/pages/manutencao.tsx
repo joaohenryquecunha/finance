f3f47786948188c2b304a5367124b14d79523a8c
João Henryque <106501423+joaohenryquecunha@users.noreply.github.com>
2025-05-19 17:12:10 -0300

    Delete src/pages/manutencao.tsx

--- src/pages/manutencao.tsx
+++
@@ -1,9 +0,0 @@
-// pages/manutencao.tsx
-export default function Manutencao() {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '5rem' }}>
-      <h1>🛠️ Em Manutenção</h1>
-      <p>Estamos trabalhando em melhorias. Volte em breve!</p>
-    </div>
-  );
-}
